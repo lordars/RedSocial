e0f2aa1cc909fb175a6d3d45ef5c066cebdad742
Adrián Ricardo Iazbeck Scalia <adrian.r.scalia@gmail.com>
2022-06-18 07:24:59 -0300

    primer start

---
+++ src/pages/home/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+export default function header() {
+  return (
+    <header className='header'>
+    <div className='toolbar'>   
+        <div className=''>
+            <span>
+                Red Social
+            </span>
+
+        </div>
+        
+        <div className=''>
+            <button>
+                novo post
+            </button>
+            <span>img1</span>
+            <span>img2</span>
+        </div>
+
+
+    </div>
+</header>
+  )
+}
---
+++ src/pages/home/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./style.css"
+import Header  from "./components/Header/Header"
+
+
+export default function home() {
+  return (
+    <div>
+        <Header/>
+        <main className='main'>
+            <div className='navbar'>nvabar</div>
+            <div className='feed'>feeed</div>
+        </main>
+    </div>
+  )
+}
